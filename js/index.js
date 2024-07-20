dd6925d43b6c5b1db832e864788c0811d9ecfaf0
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-07-20 18:24:06 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,185 @@
+let a = 10;
+res = 0;
+c = 8;
+b = -1;
+d = 1;
+//1
+// if (a > 0) {
+//   res = a + 1;
+// }
+// console.log(res);
+
+//2
+// if (a > 0) {
+//   res = a + 1;
+// } else {
+//   res = a - 2;
+// }
+// console.log(res);
+
+//3
+// if (a > 0) {
+//   res = a + 1;
+// } else if (a == 0) {
+//   res = 10;
+// } else {
+//   res = a - 2;
+// }
+// console.log(res);
+
+//4
+
+// if (a > 0) {
+//   res = res + 1;
+// }
+// if (b > 0) {
+//   res = res + 1;
+// }
+// if (d > 0) {
+//   res = res + 1;
+// }
+// console.log(res);
+
+// 5
+// let plus = 0;
+// let minus = 0;
+
+// if (a > 0) {
+//   plus = plus + 1;
+// } else {
+//   minus = minus + 1;
+// }
+// if (b > 0) {
+//   plus = plus + 1;
+// } else {
+//   minus = minus + 1;
+// }
+// if (d > 0) {
+//   plus = plus + 1;
+// } else {
+//   minus = minus + 1;
+// }
+// console.log(plus);
+// console.log(minus);
+
+//6
+// if (a > b) {
+//   res = a;
+// } else {
+//   res = b;
+// }
+// console.log(res);
+
+//7
+// if (a > b) {
+//   res = 2;
+// } else {
+//   res = 1;
+// }
+// console.log(res);
+
+//8
+// let max = 0;
+// let min = 0;
+// if (a > b) {
+//   max = a;
+//   min = b;
+// } else {
+//   max = b;
+//   min = a;
+// }
+// console.log(max);
+// console.log(min);
+
+//9
+// i = 0;
+// if (a > d) {
+//   i = a;
+//   a = d;
+//   d = i;
+// }
+// console.log(a + d);
+
+// 10
+
+// if (a !== b) {
+//   res = a + b;
+// } else {
+//   res = 0;
+// }
+
+// console.log(res);
+
+//11
+// if (a !== b) {
+//   if (a > b) {
+//     res = a;
+//   } else {
+//     res = b;
+//   }
+// } else {
+//   res = 0;
+// }
+
+console.log(res);
+
+// 12
+
+// let min;
+
+// if (a <= b && a <= c) {
+//   min = a;
+// } else if (b <= a && b <= c) {
+//   min = b;
+// } else {
+//   min = c;
+// }
+
+// console.log(min);
+
+// 13
+
+// let ortacha;
+
+// if ((a >= b && a <= c) || (a <= b && a >= c)) {
+//   ortacha = a;
+// } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+//   ortacha = b;
+// } else {
+//   ortacha = c;
+// }
+
+// console.log(ortacha);
+// 14
+// let kichigi, kattasi
+
+// if (a <= b && a <= c) {
+//   kichigi = a
+//   kattasi = b > c ? b : c
+// } else if (b <= a && b <= c) {
+//   kichigi = b
+//   kattasi = a > c ? a : c
+// } else {
+//   kichigi = c
+//   kattasi = a > b ? a : b
+// }
+
+// console.log(kichigi)
+// console.log(kattasi)
+
+// 15
+// (a = 10), (b = 20), (c = 30);
+// let sum1, sum2;
+
+// if (a + b > a + c && a + b > b + c) {
+//   sum1 = a;
+//   sum2 = b;
+// } else if (a + c > a + b && a + c > b + c) {
+//   sum1 = a;
+//   sum2 = c;
+// } else {
+//   sum1 = b;
+//   sum2 = c;
+// }
+
+// console.log(sum1, sum2);
